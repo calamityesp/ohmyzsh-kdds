d5f7e43917baa14486ee1b5bbfe0f262c035ea6b
calamityesp <calamity_xemus@hotmail.com>
2025-12-16 19:16:05 -0700

    adding another brew check location

--- custom/tools/bootstrap-kdds-zsh.sh
+++ custom/tools/bootstrap-kdds-zsh.sh
@@ -86,6 +86,8 @@ if [[ -x "/opt/homebrew/bin/brew" ]]; then
    eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ -x "/usr/local/bin/brew" ]]; then
    eval "$(/usr/local/bin/brew shellenv)"
+elif [[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else
    printf "✘ Homebrew not found. Installing\n"
    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
