f6be5b14c5c392d804bbac657604630edd21dccc
calamityesp <calamity_xemus@hotmail.com>
2025-12-20 20:33:00 -0700

    Adding the scripts I wrote on the work profile

--- custom/tools/setup-oh-my-zsh-kdds.sh
+++ custom/tools/setup-oh-my-zsh-kdds.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 ####################################################
 # Script: setup-oh-my-zsh-kdds.sh
 #
--- custom/tools/update_dotfiles.sh
+++ custom/tools/update_dotfiles.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 Dotfiles="$HOME/.oh-my-zsh-kdds/Dotfiles"
-
+git -C $Dotfiles pull
+git -C $Dotfiles submodule update --remote
+git -C $HOME/.oh-my-zsh-kdds/ submodule update --remote
 for dir in $Dotfiles/*; do 
   base=$(basename $dir)
   stow -d $Dotfiles -t ~ $base
