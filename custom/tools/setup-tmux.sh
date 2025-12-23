095f8047554a2ea8b65c7fb583c8d8a7a9442e23
calamityesp <calamity_xemus@hotmail.com>
2025-12-22 19:54:25 -0700

    modifyhing tmux config

--- custom/tools/setup-tmux.sh
+++ custom/tools/setup-tmux.sh
@@ -6,21 +6,7 @@
 
 set -euo pipefail
 
-# ---------- globals ----------
-# VARS ONLY
-
-# ---------- functions ----------
-# helper functions only
-
-# ---------- checks ----------
-# sudo / OS / prerequisites
-
 # ---------- main ----------
 printf "Cloning tmux-power...\n"
-git $ZSH/Repos/ clone git@github.com:wfxr/tmux-power.git
+git -C $ZSH/Repos/ clone git@github.com:wfxr/tmux-power.git
 source ~/.tmux.conf
-
-# ---------- exit ----------
-# final message / cleanup
-
-
